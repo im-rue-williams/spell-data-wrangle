a79136c692d4183d0bb6ff52459c8b7714355019
rue <imruewilliams@gmail.com>
2024-01-24 19:55:17 -0800

    parsing html for props not in json

--- spell.py
+++ spell.py
@@ -1,21 +1,22 @@
 class Spell:
-    def __init__(self, spell_item):
-        self.name = spell_item['name']
-        self.school = spell_item['school']
-        self.verbal_comp = spell_item['verbal']
-        self.somatic_comp = spell_item['somatic']
-        self.material_comp = spell_item['material']
-        self.materials = spell_item['materials']
-        self.range = spell_item['range']
-        self.aoe = spell_item['aoe']
-        self.casting_time = spell_item['castingTime']
-        self.duration = spell_item['duration']
-        self.save = spell_item['save']
-        self.damage = spell_item['damage']
-        self.description = spell_item['description']
-        self.aoe = spell_item['aoe']
+    def __init__(self):
+        self.id = 0
+        self.name = ''
+        self.school = ''
+        self.verbal_comp = True
+        self.somatic_comp = True
+        self.material_comp = True
+        self.materials =  ''
+        self.range =  ''
+        self.aoe =  ''
+        self.casting_time =  ''
+        self.duration =  ''
+        self.save =  ''
+        self.damage =  ''
+        self.description =  ''
         self.level = 0
-        self.residue = 'Uncommon'
-        self.source = 'PHB pg 170'
-        self.sphere = 'animal'
+        self.residue = ''
+        self.source = ''
+        self.sphere = ''
+        self.caster = ''
 
