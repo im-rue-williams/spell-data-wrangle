ecdf210a7f45d3259af6792f5dc9753d62205eaa
rue <imruewilliams@gmail.com>
2024-01-29 17:33:15 -0800

    added functions to get cate4gory list of categories

--- grab_id_by_sphere.py
+++ grab_id_by_sphere.py
@@ -1,4 +1,3 @@
-import urllib.request
 from utility import *
 
 spheres = [
@@ -9,10 +8,10 @@ spheres = [
     'Combat', 
     'Creation', 
     'Divination', 
-    'Elemental air', 
-    'Elemental earth', 
-    'Elemental fire', 
-    'Elemental water',
+    'Elemental%20Air', 
+    'Elemental%20Earth', 
+    'Elemental%20Fire', 
+    'Elemental%20Water',
     'Guardian',
     'Healing',
     'Law',
@@ -29,18 +28,8 @@ spheres = [
     'Wards',
     'Weather'
 ]
-sphere_ids = {}
-url = 'https://regalgoblins.com/spells.php?sphere='
-
-with urllib.request.urlopen(url + spheres[1]) as response:
-    html = response.read()
-
-sphere_ids[spheres[1]] = []
-spell_list = parse_spell_list(html)
-spellbook = [] 
-for spell_div in spell_list:
-    spellbook.append(create_spell(spell_div))
-for spell in spellbook:
-    sphere_ids[spheres[1]].append(spell.id)
+sphere_ids ={}
+for category in spheres:
+   sphere_ids[category] = get_spell_ids(category) 
 
 print(sphere_ids)
--- utility.py
+++ utility.py
@@ -1,14 +1,19 @@
+import urllib.request
 from bs4 import BeautifulSoup
 from spell import Spell
 
-def parse_spell_list(f):
+def parse_spell_html(f):
     soup = BeautifulSoup(f, 'html.parser')
     spells = soup.find_all('div', attrs={'data-id' : True})
     return spells
+def get_spell_html(category):
+    url = 'https://regalgoblins.com/spells.php?sphere='
+    with urllib.request.urlopen(url + category) as response:
+        return parse_spell_html(response.read())
 
 def create_spell(spell_div):
     spell = Spell()
-    spell.id = spell_div['data-id']
+    spell.id = get_id(spell_div)
     spell.school = spell_div['data-school']
     spell.caster = spell_div['data-caster']
     spell.level = spell_div.find('button', {'class' : 'level'}).text
@@ -27,3 +32,14 @@ def create_spell(spell_div):
     spell.verbal_comp = bool(spell_div.find('div', {'class' : 'verbal'}))
     spell.material_comp = bool(spell_div.find('div', {'class' : 'material'}))
     return spell
+
+def get_id (spell_div):
+    return spell_div['data-id']
+
+def get_spell_ids(category):
+    spell_list = get_spell_html(category)
+    spell_ids = []
+    for spell_div in spell_list:
+        spell_ids.append(get_id(spell_div))
+
+    return spell_ids
